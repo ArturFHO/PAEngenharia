d69106096755fad0a0b5013c7844bba05ae0a372
ArturFHO <artur.fernando@alunos.fho.edu.br>
2022-04-01 20:50:15 -0300

    alterações na DAO

--- PA/PA/DAO/ClienteDAO.cs
+++ PA/PA/DAO/ClienteDAO.cs
@@ -16,7 +16,7 @@ namespace PA.DAO
             comando.CommandType = System.Data.CommandType.Text;
             comando.CommandText = 'INSERT INTO Cliente (nome_cliente, cpf_cliente, tel_cliente, cel_cliente, email_cliente, end_cliente) VALUES (@nome, @cpf, @telefone, @celular, @email, @endereço)';
 
-            comando.Parameters.AddWithValue("@nome", cliente.)
+            comando.Parameters.AddWithValue("@nome", cliente);
             comando.Parameters.AddWithValue("@nome", cliente);
             comando.Parameters.AddWithValue("@nome", cliente);
             comando.Parameters.AddWithValue("@nome", cliente);
@@ -24,6 +24,52 @@ namespace PA.DAO
             comando.Parameters.AddWithValue("@nome", cliente);
             comando.Parameters.AddWithValue("@nome", cliente);
 
+            ConexaoBanco.CRUD(comando);
+        }
+
+        public void Update(Cliente cliente)
+        {
+            NpgsqlCommand comando = new NpgsqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = 'INSERT INTO Cliente (nome_cliente, cpf_cliente, tel_cliente, cel_cliente, email_cliente, end_cliente) VALUES (@nome, @cpf, @telefone, @celular, @email, @endereço)';
+
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+
+            ConexaoBanco.CRUD(comando);
+
+        }
+
+        public void Delete(Cliente cliente)
+        {
+            NpgsqlCommand comando = new NpgsqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = 'INSERT INTO Cliente (nome_cliente, cpf_cliente, tel_cliente, cel_cliente, email_cliente, end_cliente) VALUES (@nome, @cpf, @telefone, @celular, @email, @endereço)';
+
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+            comando.Parameters.AddWithValue("@nome", cliente);
+
+            ConexaoBanco.CRUD(comando);
+        }
+
+        public void BuscarPorId(int id)
+        {
+            NpgsqlCommand comando = new NpgsqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = "SELECT * FROM Cliente WHERE id_cliente=@id";
+            comando.Parameters.AddWithValue("@id");
+            
+            ConexaoBanco.CRUD(comando);
         }
     }
 }
